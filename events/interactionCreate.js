56d792698a4f1be0d9519a2c8a1c032dec9c8f0e
TheRavenSeb <cdawgthomas@gmail.com>
2026-03-12 13:55:05 -0400

    fix: correct attendee field index in event message embed

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -26,7 +26,7 @@ if (interaction.isButton()) {
 	const channel = await client.channels.fetch("1462570082793160867");
 	const message = await channel.messages.fetch(event.messageId);
 	const Event = EmbedBuilder.from(message.embeds[0])
-	.spliceFields(0, 1, { name: "Attendees", value: event.attendees.map(a => a.username).join("\n") || "No attendees yet", inline: false })
+	.spliceFields(6, 1, { name: "Attendees", value: event.attendees.map(a => a.username).join("\n") || "No attendees yet", inline: false })
 	.setFooter({ text: `${event.attendees.length} attendees` });
 	message.edit({ embeds: [Event] });
 
@@ -50,8 +50,7 @@ if (interaction.isButton()) {
 	const channel = await client.channels.fetch("1462570082793160867");
 	const message = await channel.messages.fetch(event.messageId);
 	const Event = EmbedBuilder.from(message.embeds[0])
-	.spliceFields(0, 1, { name: "Attendees", value: event.attendees.map(a => a.username).join("\n") || "No attendees yet", inline: false })
-	.addFields({ name: "Attendees", value: event.attendees.map(a => a.username).join("\n") || "No attendees yet", inline: false })
+	.spliceFields(6, 1, { name: "Attendees", value: event.attendees.map(a => a.username).join("\n") || "No attendees yet", inline: false })
 	.setFooter({ text: `${event.attendees.length} attendees` });
 	message.edit({ embeds: [Event] });
 
